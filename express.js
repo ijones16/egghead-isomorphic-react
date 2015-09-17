0a78eb5543265c7958b9a8a37223be9241073ae7
ijones16 <jones58ian@gmail.com>
2015-09-17 10:35:52 -0700

    updated to react 0.14.0-rc1

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var React = require('react');
 var fs = require('fs');
+var ReactDOMServer = require('react-dom/server');
 var Component = React.createFactory(require('./Component'));
 
 var BUNDLE = fs.readFileSync('./bundle.js', {encoding: 'utf8'});
@@ -11,7 +12,9 @@ var app = express();
 function home(req, res){
   var msg = req.params.msg || 'Hello';
   var comp = Component({msg:msg});
-  var page = TEMPLATE.replace('@@@', React.renderToString(comp))
+  // React.renderToStaticMarkup is deprecated. 
+  // Please use ReactDOMServer.renderToStaticMarkup from require('react-dom/server') instead.
+  var page = TEMPLATE.replace('@@@', ReactDOMServer.renderToStaticMarkup(comp))
   page = page.replace('###', '<script>renderApp("'+msg+'")</script>')
   res.send(page);
 }
